9164d5e3ce333b4bb56cb0bff1d1d4857b7dd7f2
kakarot <kakarot666@163.com>
2021-04-09 19:48:18 +0800

    readme

--- src/main/java/com/example/demo/service/MysqlBlobService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-import com.example.demo.bean.ImgEntity;
-
-public interface MysqlBlobService {
-    Integer saveBlobImg(ImgEntity img);
-    byte[] showPicture(String id);
-    public String getFileName(String id);
-}
